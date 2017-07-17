f63907c3599fd97d6afcf3c17353efa8bfd82c16
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-17 14:51:50 -0400

    #81

---
+++ SearchRotated/src/Search.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class Search {
+    public boolean search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
