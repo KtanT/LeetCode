9df047bc3f3f0381dbd2b024cd24cdcc7be99ece
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-17 19:48:20 -0400

    #120

---
+++ Triangle/src/Triangle.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.isEmpty()) {
+            return -1;
+        }
+        
+        int size = triangle.size();
+        int[] res = new int[size + 1];
+        
+        for (int i = size - 1; i >= 0; i--) {
+            List<Integer> temp = triangle.get(i);
+            
+            for (int j = 0; j < temp.size(); j++) {
+                res[j] = Math.min(res[j], res[j+1]) + temp.get(j);
+            }
+        }
+        return res[0];
+    }
+}
