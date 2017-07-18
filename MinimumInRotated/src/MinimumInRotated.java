ffbed4c04120cb0e0f462630a609184937f7a815
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-17 20:41:27 -0400

    #153

---
+++ MinimumInRotated/src/MinimumInRotated.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class MinimumInRotated {
+    public int findMin(int[] nums) {
+        int temp = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < temp) {
+                temp = nums[i];
+            }
+        }
+        return temp;
+    }
+}
