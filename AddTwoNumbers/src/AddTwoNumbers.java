579b6cf15db34fa596dff8a28cdd0cecf85b3664
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-19 13:40:39 -0400

    #2

---
+++ AddTwoNumbers/src/AddTwoNumbers.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class AddTwoNumbers {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode p = l1, q = l2, cur = result;
+        int carry = 0;
+
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (p != null) {
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+        }
+        
+        if (carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        
+        return result.next;
+    }
+}
