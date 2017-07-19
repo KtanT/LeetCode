aa0dab5d5eec6f41754fff02522c3bf2dfd0570a
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-18 22:57:25 -0400

    #45

---
+++ JumpGame2/src/JumpGame2.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class JumpGame2 {
+    public int jump(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int jump = 0, curEnd = 0, curFarthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            curFarthest = Math.max(curFarthest, i + nums[i]);
+            if (i == curEnd) {
+                jump++;
+                curEnd = curFarthest;
+            }
+        }
+
+        return jump;
+    }
+}
