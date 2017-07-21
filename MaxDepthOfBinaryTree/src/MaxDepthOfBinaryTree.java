6c397c48e07357156a2fb270350d43011e56d39a
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-21 14:40:00 -0400

    #104

---
+++ MaxDepthOfBinaryTree/src/MaxDepthOfBinaryTree.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class MaxDepthOfBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
