b790b4d6ec62658e5efbd205b5cacc468d77d651
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-24 18:23:11 -0400

    #205

---
+++ IsomorphicStrings/src/IsomorphicStrings.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            if (map.containsKey(a)) {
+                if (map.get(a).equals(b)) {
+                    continue;
+                } else {
+                    return false;
+                }
+            } else {
+                if (!map.containsValue(b)) {
+                    map.put(a, b);
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
