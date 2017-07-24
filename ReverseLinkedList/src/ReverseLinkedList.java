922cd87adee2d4103a14fe1415917323b8d047fc
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-24 18:28:17 -0400

    #206

---
+++ ReverseLinkedList/src/ReverseLinkedList.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
