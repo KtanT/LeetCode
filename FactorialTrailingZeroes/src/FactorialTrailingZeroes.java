f76db49c25c68162623ace775bd6dc39f681a26c
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-24 14:09:27 -0400

    #172

---
+++ FactorialTrailingZeroes/src/FactorialTrailingZeroes.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int sum = 0;
+        while (n > 0) {
+            n = n / 5;
+            sum = sum + n;
+        }
+
+        return sum;
+    }
+}
