b92bb93bf308d1df6f467dc20245e9f18988635c
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-24 15:35:55 -0400

    #202

---
+++ HappyNumber/src/HappyNumber.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class HappyNumber {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        int square = 0, remain = 0;
+
+        while (set.add(n)) {
+            square = 0;
+            while (n > 0) {
+                remain = n % 10;
+                square += remain * remain;
+                n /= 10;
+            }
+
+            if (square == 1) {
+                return true;
+            } else {
+                n = square;
+            }
+        }
+
+        return false;
+    }
+}
