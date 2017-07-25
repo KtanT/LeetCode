5f0632d846e90554f7f92637b7ffb30c9b207e13
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-25 10:32:42 -0400

    #231

---
+++ PowerOfTwo/src/PowerOfTwo.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n > 0) {
+            if (n == 1) {
+                return true;
+            }
+            if (n % 2 != 0) {
+                return false;
+            } else {
+                n /= 2;
+            }
+        }
+        return true;
+    }
+}
