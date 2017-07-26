3748929f0d513d4f6a737498b7c442c5dcee9361
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-26 17:20:28 -0400

    #367

---
+++ ValidPerfectSquare/src/ValidPerfectSquare.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class ValidPerfectSquare {
+    public boolean isPerfectSquare(int num) {
+        int i = 1;
+        while (num > 0) {
+            num -= i;
+            i += 2;
+        }
+        return num == 0;
+    }
+}
