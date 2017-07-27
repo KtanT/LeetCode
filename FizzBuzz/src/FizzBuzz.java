c521959e4dd89167fabf7d4e6e148f10a99aa954
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-27 15:22:01 -0400

    #412

---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    list.add("FizzBuzz");
+                } else {
+                    list.add("Fizz");
+                }
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(Integer.toString(i));
+            }
+        }
+        return list;
+    }
+}
