ec16f28f2f70eb27f5b760a46dd71dd6079804a5
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-27 15:36:19 -0400

    #434

---
+++ NumberOfSegmentsInString/src/NumberOfSegmentsInString.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class NumberOfSegmentsInString {
+    public int countSegments(String s) {
+        if (s == null || s.trim().length() == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
