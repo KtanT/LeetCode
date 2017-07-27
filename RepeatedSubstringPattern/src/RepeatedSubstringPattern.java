eab3da3a0352504d1db8815a0e7794d7b231d92a
Kaiyuan Feng <kaiyuanf@andrew.cmu.edu>
2017-07-27 18:19:09 -0400

    #459

---
+++ RepeatedSubstringPattern/src/RepeatedSubstringPattern.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kyran
+ */
+public class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        if (s == null) {
+            return false;
+        }
+
+        int len = s.length();
+        for (int i = len / 2; i >= 1; i--) {
+            if (len % i == 0) {
+                int rep = len / i;
+                String sub = s.substring(0, i);
+                StringBuilder sb = new StringBuilder();
+
+                for (int j = 0; j < rep; j++) {
+                    sb.append(sub);
+                }
+
+                if (sb.toString().equals(s)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
